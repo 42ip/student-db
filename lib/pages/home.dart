1fdb490956e5978ab48384e4440a1f1f7a4dcf1f
Pranay0302 <pka.pranayandra02@gmail.com>
2020-08-11 11:05:57 +0530

    RFfixed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,40 +10,43 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('this is the home page'),
       ),
-      body: Column(
-        children: [
-          RaisedButton(
-            onPressed: () {},
-            child: Text('branches', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('college info', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('Exams', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.pushNamed(
-                context,
-                '/studentHome',
-              );
-            },
-            child: Text('Student', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('Subjects', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {},
+              child: Text('branches', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('college info', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Exams', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(
+                  context,
+                  '/studentHome',
+                );
+              },
+              child: Text('Student', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Subjects', style: TextStyle(fontSize: 20)),
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
+            ),
+          ],
+        ),
       ),
     );
   }
