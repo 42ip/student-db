d20632b123438589f9f7e655dfd33f55f5bc2e41
santoshpreetam <preetamsantosh007@gmail.com>
2020-08-11 11:39:35 +0530

    centered and added padding to the buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,39 +16,59 @@ class _HomeState extends State<Home> {
         title: Text('this is the home page'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            RaisedButton(
-              onPressed: () {},
-              child: Text('branches', style: TextStyle(fontSize: 20)),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('college info', style: TextStyle(fontSize: 20)),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Exams', style: TextStyle(fontSize: 20)),
-            ),
-            RaisedButton(
-              onPressed: () {
-                Navigator.pushNamed(
-                  context,
-                  '/studentHome',
-                );
-              },
-              child: Text('Student', style: TextStyle(fontSize: 20)),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Subjects', style: TextStyle(fontSize: 20)),
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('branches', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('college info', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Exams', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/studentHome',
+                    );
+                  },
+                  child: Text('Student', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Subjects', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Disabled Button', style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/optionPages/studentHome.dart
+++ lib/pages/optionPages/studentHome.dart
@@ -19,38 +19,52 @@ class _studentHomeState extends State<studentHome> {
         backgroundColor: Colors.black,
         title: Text('this is the student home page'),
       ),
-      body: Column(
-        children: [
-          RaisedButton(
-            onPressed: () async {
-              var client = new http.Client();
-              final res =
-                  await client.get('http://34.224.4.55:8080/getAllStudents');
-              print(res.body);
-              final parsed =
-                  await json.decode(res.body).cast<Map<String, dynamic>>();
-              items = await parsed
-                  .map<Student>((json) => Student.fromJson(json))
-                  .toList();
-              print(items);
-              Navigator.pushNamed(context, '/getAllstudents',
-                  arguments: {'items': items});
-            },
-            child: Text('get', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('insert', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('update', style: TextStyle(fontSize: 20)),
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text('delete', style: TextStyle(fontSize: 20)),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () async {
+                  var client = new http.Client();
+                  final res =
+                      await client.get('http://34.224.4.55:8080/getAllStudents');
+                  print(res.body);
+                  final parsed =
+                      await json.decode(res.body).cast<Map<String, dynamic>>();
+                  items = await parsed
+                      .map<Student>((json) => Student.fromJson(json))
+                      .toList();
+                  print(items);
+                  Navigator.pushNamed(context, '/getAllstudents',
+                      arguments: {'items': items});
+                },
+                child: Text('get', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('insert', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('update', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('delete', style: TextStyle(fontSize: 20)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
